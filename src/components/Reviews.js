4755c8c83da0547814efdd3c0a546224d1412aa2
chabadilla92 <chabadilla92@gmail.com>
2022-03-23 11:19:24 -0700

    reviews done

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -6,46 +6,51 @@ function Reviews() {
             <div class="container-fluid">
                 <div class="row">
                     <h1 class="text-center font-weight-bold" id="reviewsheader">REVIEWS</h1>
-                    <p class="text-center">We do everything we can to provide the best experience</p>
+                    <p class="text-center">Here's what some of our guests had to say about us</p>
                 </div>
                 <div class="row">
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>I was so impressed by everything. The prosciutto broth on the way to the table was nothing short of heavenly. The scallop was terrific. The oysters also grand. The abalone was the least impressive, but not bad at all! The duck pasta was so good. I may have gotten a little teary eyed. The truffle pasta was also great. I loved the escargot steak and the aged fillet.</p>
+                        <h2>Natassia G.</h2>
 
 
                     </div>
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>2nd time here for the new pasta SOPRESSINI!! Love it!!  Small portion homemade FRESH pasta and very buttery.</p>
+                        <h2>Cherry W.</h2>
 
                     </div>
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>A late celebration but nonetheless delightful and special!
+Michelin-starred chef Jeffrey Stout has once again brought his talents to our local restaurant scene. An Italian-influenced steakhouse with beautiful decor, creative cocktails, amazing food and impeccable personal service.
+Just as the name implies, it is clearly for meat-lovers, but although a pescatarian like me would say it's worth making an exception.
+A real standout was my dirty martini, which came in a glass, on a tray, with a mirco-pitcher of more "dirty" juice.
+
+All around a superb experience!</p>
+                        <h2>Trevor N.</h2>
 
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>Definitely one of the best restaurants I been to in a long time.  My new favorite restaurant in the South Bay I would say for sure.  I highly recommend this place to anyone.</p>
+                        <h2>Mark P.</h2>
 
                     </div>
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>Top-notch service, cool ambiance, and great menu. This is definitely going onto our list of great special-occasion places.</p>
+                        <h2>April Y.</h2>
 
                     </div>
                     <div class="col-md-4 border border-dark p-2 mt-2">
                         <h4><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></h4>
-                        <p>Here are the reviews</p>
-                        <h2>Andrew Thai</h2>
+                        <p>OH MY GOD I LOVE THIS PLACE! Their customer service is top notch.</p>
+                        <h2>Amy P.</h2>
 
                     </div>
                 </div>
